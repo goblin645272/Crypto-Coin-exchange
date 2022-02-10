7c468fbc2c7796db6494694df0f2d9a6b69bc6f0
goblin645272 <75854892+goblin645272@users.noreply.github.com>
2022-02-10 11:33:02 +0530

    Frontend Link

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,99 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Web3 from "web3";
+import Navbar from "./Navbar";
+import EthSwap from "../abis/EthSwap.json";
+import Token from "../abis/Token.json";
 
 class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+    this.setState({ account: accounts[0] });
+    console.log(this.state.account);
+    const ethBalance = await web3.eth.getBalance(this.state.account);
+    this.setState({ ethBalance });
+    // Load Token
+
+    const networkId = await web3.eth.net.getId();
+    const tokenData = Token.networks[networkId];
+    if (tokenData) {
+      const token = new web3.eth.Contract(Token.abi, tokenData.address);
+      this.setState({ token });
+
+      let tokenBalance = await token.methods
+        .balanceOf(this.state.account)
+        .call();
+
+      console.log(this.state);
+      console.log(tokenBalance);
+      this.setState({ tokenBalance: tokenBalance.toString() });
+    } else {
+      window.alert("Token contract not deployed to detected network.");
+    }
+
+    // Load EthSwap
+    const ethSwapData = EthSwap.networks[networkId];
+    if (ethSwapData) {
+      const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address);
+      this.setState({ ethSwap });
+    } else {
+      window.alert("EthSwap contract not deployed to detected network.");
+    }
+
+    this.setState({ loading: false });
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  }
+
+  buyTokens = etherAmount => {
+    this.setState({ loading: true });
+    this.state.ethSwap.methods
+      .buyTokens()
+      .send({ value: etherAmount, from: this.state.account })
+      .on("transactionHash", hash => {
+        this.setState({ loading: false });
+      });
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: "",
+      token: {},
+      ethSwap: {},
+      ethBalance: "0",
+      tokenBalance: "0",
+      loading: true
+    };
+  }
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-            Mischevious Goblin
-        </nav>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <h>Hello</h>
+                <h1>Hello</h1>
               </div>
             </main>
           </div>
